9f6c6377bd2ca7730ddc56ef0c9dd4551658468e
RelexSun <88622289+RelexSun@users.noreply.github.com>
2023-11-22 22:56:45 +0700

    create get api endpoint

--- src/server.js
+++ src/server.js
@@ -6,3 +6,20 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Running express server on port ${PORT}!!!`);
 });
+
+app.get("/groceries", (req, res) => {
+  res.send([
+    {
+      item: "milk",
+      quantity: 2,
+    },
+    {
+      item: "cereal",
+      quantity: 1,
+    },
+    {
+      item: "coke",
+      quantity: 3,
+    },
+  ]);
+});
