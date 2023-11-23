b558ede13ff8a8009e3c1f7f906477ae5b0107fe
RelexSun <relexsunnop@gmail.com>
2023-11-23 14:45:27 +0700

    set up middleware

--- src/server.js
+++ src/server.js
@@ -3,23 +3,42 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+// this middleware allow us to send json to our server. Parsing data
+app.use(express.json());
+
+// this middleware allow us to request body in URL-encoded format.  Parsing data
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.url}`);
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Running express server on port ${PORT}!!!`);
 });
 
+const groceriesList = [
+  {
+    item: "milk",
+    quantity: 2,
+  },
+  {
+    item: "cereal",
+    quantity: 1,
+  },
+  {
+    item: "coke",
+    quantity: 3,
+  },
+];
+
 app.get("/groceries", (req, res) => {
-  res.send([
-    {
-      item: "milk",
-      quantity: 2,
-    },
-    {
-      item: "cereal",
-      quantity: 1,
-    },
-    {
-      item: "coke",
-      quantity: 3,
-    },
-  ]);
+  res.send(groceriesList);
+});
+
+app.post("/groceries", (req, res) => {
+  console.log(req.body);
+  groceriesList.push(req.body);
+  res.send(201);
 });
